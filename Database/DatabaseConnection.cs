dd639dbfaf4ae5e0b0597bb1d0c16c2098d5107d
trono-wq <aerotrono@gmail.com>
2026-06-14 18:34:24 +0800

    Update database connection string password

--- Database/DatabaseConnection.cs
+++ Database/DatabaseConnection.cs
@@ -5,7 +5,7 @@ namespace HRApplicantProcessSystem.Database
     public class DatabaseConnection
     {
         private string connectionString =
-            "server=localhost;database=hr_applicant_process_window_application;uid=root;pwd=tang1nangf1nalpr0j3ctn1t0####;";
+            "server=localhost;database=hr_applicant_process_window_application;uid=root;pwd=GROUPnine1!;";
 
         public MySqlConnection GetConnection()
         {
