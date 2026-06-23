890e2955f99dbc8574c9d48622bb3e2a75e19a1b
trono-wq <aerotrono@gmail.com>
2026-06-23 17:32:39 +0800

    Update frmMyApplication.cs

--- frmMyApplication.cs
+++ frmMyApplication.cs
@@ -237,9 +237,10 @@ namespace COMP_003_CAPSTONE
                         ON a.applicant_id = ap.applicant_id
                     INNER JOIN JobVacancies j
                         ON a.job_vacancy_id = j.job_vacancy_id
-                    WHERE ap.applicant_account_id = @accountId
                     INNER JOIN PositionTypes p
                         ON j.position_type_id = p.position_type_id    
+                    WHERE ap.applicant_account_id = @accountId
+                    
                     AND p.position_type_name = @position    
                     LIMIT 1";
   
